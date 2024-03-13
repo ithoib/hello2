66fc87a984d8dd83f82bdad8cec5e4596e544b90
awhonma <155790847+awhonma@users.noreply.github.com>
2024-03-13 14:59:06 +0700

    hapus hello

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package hello;
-
+import java.util.Arrays;
 /**
  *
  * @author RPL
@@ -15,7 +15,18 @@ public class Hello {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-
+        int arrayAngka[][] = {{1,2,3,4,5,6,7,8}, {1,2,3,4,5,6,7,8}};
+        
+        arrayAngka[1][3] = 10;
+        
+        // for standar
+        for (int i=0; i<arrayAngka.length; i++){
+            for(int j=0; j<arrayAngka[i].length; j++){
+                System.out.print(arrayAngka[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+        
     }
     
 }
